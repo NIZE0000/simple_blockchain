e4a237b8f3dff3984c7fb403f9ce65097f8c6a78
NIZE0000 <anupong.niceone@gmail.com>
2023-07-29 08:21:05 +0700

    add mining route

--- main.py
+++ main.py
@@ -17,14 +17,25 @@ def main():
         }
         return jsonify(response), 200
     
-    @app.route('/mining', methods=["POST"])
-    def proof_of_work():
-        blockchain.proof_of_work()
+    @app.route('/mining', methods=["GET"])
+    def mining_block():
+
+        previous_block = blockchain.get_previous_block()
+
+        nonce = blockchain.proof_of_work(previous_block["nonce"])
+
+        previous_hash = blockchain.hash(previous_block)
+
+        block = blockchain.create_block(previous_hash, nonce)
 
         response = {
-            "chain":blockchain.chain,
-            "length": len(blockchain.chain)
+            "message": "Mining Suceed",
+            "index": block["index"],
+            "timestamp": block["timestamp"],
+            "nonce": block["nonce"],
+            "previouse_hash": block["previous_hash"]
         }
+
         return jsonify(response), 200
 
     app.run()
